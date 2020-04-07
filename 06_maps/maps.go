e99baf9c75ee7dedb0c2e348d60f92dd0ca4ba26
Salih Emin <cerebrux.gr@gmail.com>
2020-04-08 00:55:15 +0300

    refactor

--- 06_maps/maps.go
+++ 06_maps/maps.go
@@ -5,39 +5,40 @@ import "fmt"
 func main() {
 
 	/*
+		Maps - Χάρτες
+
 		Τα "Map" είναι μία δομή δεδομένων η οποία αποθηκεύει
 		ζευγάρια της μορφής "κλειδί,τιμή"
-		Επίσης γνωστό ως συσχετιστικός πίνακας,
-		πίνακας κατακερματισμού ή λεξικό, οι χάρτες χρησιμοποιούνται
-		για να αναζητήσετε μια τιμή από το αντίστοιχο κλειδί της (σαν μια μικρή βάση δεδομένων).
+
+		Εναλλακτικά ονόματά του είναι
+		 - πίνακας κατακερματισμού (hash table)
+		 - ή λεξικό (dictionary)
+		 - ή χάρτης/συσχετιστικός πίνακας (map/associative array)
+
+		 είναι μία πιο ευέλικτη παραλλαγή της εγγραφής δεδομένων, στην οποία
+		 τα ζευγάρια κλειδιού-τιμής μπορούν να εισαχθούν και να διαγραφούν ελεύθερα.
+
+		οι χάρτες χρησιμοποιούνται για να αναζητήσετε μια τιμή από το αντίστοιχο κλειδί της (σαν μια μικρή βάση δεδομένων).
 
 		Η μηδενική τιμή ενός χάρτη είναι "nil". Ένας "nil" χάρτης
 		δεν έχει κλειδιά, ούτε μπορούν να προστεθούν κλειδιά.
 
 	*/
 
-	// Δημιουργήθηκε με varName := make(map[keyType] valueType)
-
-	presAge := make(map[string]int)
+	presAge := make(map[string]int) // Δημιουργήθηκε με varName := make(map[keyType] valueType)
 
 	presAge["Molybdenum"] = 42
 
 	fmt.Println(presAge["Molybdenum"])
 	fmt.Println("Τι έχουμε εδώ: ", presAge)
 
-	// Get the number of items in the Map
+	fmt.Println(len(presAge)) // Λήψη αριθμού των στοιχείων στο χάρτη
 
-	fmt.Println(len(presAge))
-
-	// The size changes when a new item is added
-
-	presAge["Technetium"] = 43
+	presAge["Technetium"] = 43 // Το μέγεθος αλλάζει όταν προστίθεται ένα νέο στοιχείο
 	fmt.Println("Νεο μήκος = ", len(presAge))
 	fmt.Println("Τι έχουμε εδώ: ", presAge)
 
-	// We can delete by passing the key to delete
-
-	delete(presAge, "Technetium")
+	delete(presAge, "Technetium") // Μπορούμε να διαγράψουμε χρησιμοποιώντας την function "delete()"
 	fmt.Println("Νεο μήκος = ", len(presAge))
 	fmt.Println("Τι έχουμε εδώ: ", presAge)
 
@@ -45,12 +46,12 @@ func main() {
 }
 
 func myCustomMap() {
-	// δημιουργούμε εναν nil ("άδειο"), έτοιμο για χρήση Map
-	m := make(map[string]int)
+
+	m := make(map[string]int)                 // δημιουργούμε έναν nil ("άδειο") Map, έτοιμο για χρήση
 	fmt.Println("Η τιμή είναι:", m["Answer"]) // άδειος άρα 0
 
-	m["Answer"] = 42
-	fmt.Println("Η τιμή είναι:", m["Answer"]) // παίρνει τιμή 42
+	m["Answer"] = 42 // παίρνει τιμή 42
+	fmt.Println("Η τιμή είναι:", m["Answer"])
 
 	m["Answer"] = 48
 	timi := m["Answer"]                           // αντιγράφω την τιμή σε μια μεταβλητή "timi"
@@ -62,13 +63,15 @@ func myCustomMap() {
 
 	/*
 		Ελέγξτε ότι υπάρχει ένα κλειδί με μια εκχώρηση δύο τιμών:
-		elem, ok = m[key]
+
+				elem, ok = m[key]
+
 		Αν το κλειδί είναι σε m, είναι αληθές.
 		Αν όχι, το ok είναι ψευδές.
 
-		Αν το κλειδί δε βρίσκεται στον χάρτη,
+		Αν το κλειδί δε βρίσκεται στο map,
 		τότε το elem είναι η μηδενική τιμή για τον τύπο του στοιχείου του χάρτη.
 	*/
-	v, ok := m["Answer"]
-	fmt.Println("Η τιμή:", v, "Present?", ok)
+	elem, ok := m["Answer"]
+	fmt.Println("Η τιμή:", elem, "Υπάρχει;", ok)
 }
