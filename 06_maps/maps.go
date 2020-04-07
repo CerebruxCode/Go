276572e2b36d294b28f67601f8145fa933c66e0e
Salih Emin <cerebrux.gr@gmail.com>
2020-04-08 02:20:42 +0300

    merge maps

--- 06_maps/maps.go
+++ 06_maps/maps.go
@@ -74,4 +74,23 @@ func myCustomMap() {
 	*/
 	elem, ok := m["Answer"]
 	fmt.Println("Η τιμή:", elem, "Υπάρχει;", ok)
+
+	// Μπορούμε επίσης να αποθηκεύσουμε πολλαπλά στοιχεία σε ένα χάρτη
+	superhero := map[string]map[string]string{
+		"Superman": map[string]string{
+			"realname": "Clark Kent",
+			"city":     "Metropolis",
+		},
+
+		"Batman": map[string]string{
+			"realname": "Bruce Wayne",
+			"city":     "Gotham City",
+		},
+	}
+
+	// Μπορούμε να εξάγουμε δεδομένα όπου το κλειδί ταιριάζει με το Superman
+
+	if temp, hero := superhero["Superman"]; hero {
+		fmt.Println(temp["realname"], temp["city"])
+	}
 }
